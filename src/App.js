d3caa4efbdaf4ad304bdc5e4901b46e15920463f
unknown <mallavarapurambabu143@gmail.com>
2023-11-06 12:51:04 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useSelector,useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { Add } from '.';
 function App() {
+  const [inputs, setInputs] = useState({});
+  let dispatch = useDispatch()
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({type:Add,payload:inputs})
+    console.log(inputs);
+  }
+  let location = useSelector(s=>s)
+  console.log(location.details)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <form onSubmit={handleSubmit}>
+      <label>Enter your name:
+      <input 
+        type="text" 
+        name="name" 
+        value={inputs.name || ""} 
+        onChange={handleChange}
+      />
+      </label>
+      <label>Enter your age:
+        <input 
+          type="number" 
+          name="age" 
+          value={inputs.age || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <label>Enter your phone:
+        <input 
+          type="number" 
+          name="phone" 
+          value={inputs.phone || ""} 
+          onChange={handleChange}
+        />
+        <label>Enter your email:
+        <input 
+          type="text" 
+          name="email" 
+          value={inputs.email || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        </label>
+        <input type="submit" />
+    </form>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+let initial = {
+  details: [],
+};
+
+//
+export const Add = 'add';
+
+
+function reducer(state = initial, action) {
+  if (action.type === Add) {
+    return {
+      ...state,
+      details: [...state.details, action.payload],
+    };
+  }
+  return state;
+}
+
+
+const store = createStore(reducer);
+console.log("store",store.getState())
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> {/* Fix the typo here */}
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
